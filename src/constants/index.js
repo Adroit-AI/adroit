480c4afd5860531546cdace4196007eb027dd9ec
Adroit-AI <152613537+Adroit-AI@users.noreply.github.com>
2023-12-13 22:49:58 +0530

    Add files via upload

---
+++ src/constants/index.js
@@ -0,0 +1,169 @@
+import { people01, people02, people03, facebook, instagram, linkedin, twitter, airbnb, binance, coinbase, dropbox, send, shield, star  } from "../assets";
+
+export const navLinks = [
+  {
+    id: "home",
+    title: "Home",
+  },
+  {
+    id: "features",
+    title: "Features",
+  },
+  {
+    id: "product",
+    title: "Product",
+  },
+  {
+    id: "clients",
+    title: "Clients",
+  },
+];
+
+export const features = [
+  {
+    id: "feature-1",
+    icon: star,
+    title: "Rewards",
+    content:
+      "The best advertisements at the best prices",
+  },
+  {
+    id: "feature-2",
+    icon: shield,
+    title: "100% Secured",
+    content:
+      "We take proactive steps make sure your information is secure.",
+  },
+  {
+    id: "feature-3",
+    icon: send,
+    title: "Balance Transfer",
+    content:
+      "We make sure that all our queries are fulfilled .",
+  },
+];
+
+export const feedback = [
+  {
+    id: "feedback-1",
+    content:
+      "blah blah blah lorem ipsum boht acchi company hai, mmmm bwqrblvbckdwk jhfejwkk ekjfwblwb.",
+    name: "generic name",
+    title: "Founder & Leader",
+    img: people01,
+  },
+  {
+    id: "feedback-2",
+    content:
+      "blah blah blah lorem ipsum boht acchi company hai, mmmm bwqrblvbckdwk jhfejwkk ekjfwblwb.",
+    name: "generic name",
+    title: "Founder & Leader",
+    img: people02,
+  },
+  {
+    id: "feedback-3",
+    content:
+      "blah blah blah lorem ipsum boht acchi company hai, mmmm bwqrblvbckdwk jhfejwkk ekjfwblwb.",
+    name: "generic name",
+    title: "Founder & Leader",
+    img: people03,
+  },
+];
+
+export const stats = [
+  {
+   
+];
+
+export const footerLinks = [
+  {
+    title: "Useful Links",
+    links: [
+      {
+        name: "Content",
+        link: "https://www.adroit.com/content/",
+      },
+      {
+        name: "How it Works",
+        link: "https://www.adroit.com/how-it-works/",
+      },
+      {
+        name: "Create",
+        link: "https://www.adroit.com/create/",
+      },
+      {
+        name: "Explore",
+        link: "https://www.adroit.com/explore/",
+      },
+      {
+        name: "Terms & Services",
+        link: "https://www.adroit.com/terms-and-services/",
+      },
+    ],
+  },
+  {
+    title: "Community",
+    links: [
+      {
+        name: "Help Center",
+        link: "https://www.adroit.com/help-center/",
+      },
+      {
+        name: "Partners",
+        link: "https://www.adroit.com/partners/",
+      },
+      {
+        name: "Suggestions",
+        link: "https://www.adroit.com/suggestions/",
+      },
+      {
+        name: "Blog",
+        link: "https://www.adroit.com/blog/",
+      },
+      {
+        name: "Newsletters",
+        link: "https://www.adroit.com/newsletters/",
+      },
+    ],
+  },
+  {
+    title: "Partner",
+    links: [
+      {
+        name: "Our Partner",
+        link: "https://www.adroit.com/our-partner/",
+      },
+      {
+        name: "Become a Partner",
+        link: "https://www.adroit.com/become-a-partner/",
+      },
+    ],
+  },
+];
+
+export const socialMedia = [
+  {
+    id: "social-media-1",
+    icon: instagram,
+    link: "https://www.instagram.com/",
+  },
+  {
+    id: "social-media-2",
+    icon: facebook,
+    link: "https://www.facebook.com/",
+  },
+  {
+    id: "social-media-3",
+    icon: twitter,
+    link: "https://www.twitter.com/",
+  },
+  {
+    id: "social-media-4",
+    icon: linkedin,
+    link: "https://www.linkedin.com/",
+  },
+];
+
+export const clients = [
+  
+];
